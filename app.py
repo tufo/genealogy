c0b0c138d3e2228b8a8cd5cae2df991fc7be33cc
tufo <cal.tufo@gmail.com>
2024-11-04 14:46:40 -0500

    changes to import

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 
 import os
 
-from cs50 import SQL
+# from cs50 import SQL
+import sqlite3
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
